85eec49e5663846a603038fb3cdafb5f7846808d
ridwantaufk <ridwan1998taufik@gmail.com>
2025-04-22 03:23:15 +0700

    commit pertama saya pembuatan SIMRS skala enterprise

---
+++ simrs-backend/src/main/java/com/example/simrs/SimrsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.simrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimrsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimrsBackendApplication.class, args);
+	}
+
+}
---
+++ simrs-backend/src/test/java/com/example/simrs/SimrsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimrsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
